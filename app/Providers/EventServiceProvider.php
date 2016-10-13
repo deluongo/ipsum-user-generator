c770e33528abe35a092df371b4feac7fafe5e4bf
deluongo <deluongo@gmail.com>
2016-10-12 22:00:07 -0700

    First Commit

---
+++ app/Providers/EventServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace p3\Providers;
+
+use Illuminate\Support\Facades\Event;
+use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
+
+class EventServiceProvider extends ServiceProvider
+{
+    /**
+     * The event listener mappings for the application.
+     *
+     * @var array
+     */
+    protected $listen = [
+        'p3\Events\SomeEvent' => [
+            'p3\Listeners\EventListener',
+        ],
+    ];
+
+    /**
+     * Register any events for your application.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+    }
+}

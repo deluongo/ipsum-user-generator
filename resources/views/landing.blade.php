b962f7fcbf8bd5697f486035f1201342d3d3de91
deluongo <deluongo@gmail.com>
2016-10-19 22:51:21 -0700

    Fix CSS

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,13 +5,15 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Developer Tools</title>
 
+        <!-- Styles -->
+        <link href="{{ asset('/css/landing.css') }}" rel="stylesheet">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <link rel="stylesheet" type="text/css" href="/css/landing.css">
+
+
     </head>
     <body>
       <section class="c-hero-flex">
